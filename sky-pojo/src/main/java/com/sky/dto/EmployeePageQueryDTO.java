6c735b0f3da5f03bfbe6bcad127216581f4d47fc
David <3205460544@qq.com>
2026-04-03 16:00:18 +0800

    员工分页查询实现

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,19 +1,23 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@ApiModel("分页查询时创建的数据模型")
 @Data
 public class EmployeePageQueryDTO implements Serializable {
 
-    //员工姓名
+    @ApiModelProperty("员工姓名")
     private String name;
 
-    //页码
+    @ApiModelProperty("页码")
     private int page;
 
-    //每页显示记录数
+    @ApiModelProperty("每页显示记录数")
     private int pageSize;
 
 }
