05f7631143dd892f4d381f595a0cfe8c04a683ad
David <3205460544@qq.com>
2026-04-06 22:31:19 +0800

    实现绕过微信支付 来完成支付功能

--- sky-server/src/main/java/com/sky/controller/admin/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/user/OrderController.java
@@ -1,18 +1,17 @@
 package com.sky.controller.admin.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.impl.OrderServiceImpl;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "UserOrderController")
 @RequestMapping("/user/order")
@@ -25,8 +24,24 @@ public class OrderController {
     @PostMapping("/submit")
     @ApiOperation("用户端提交接口")
     public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
-        log.info("cC端用户提交订单");
+        log.info("C端用户提交订单");
         OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
     }
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("订单支付：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("生成预支付交易单：{}", orderPaymentVO);
+        orderService.paySuccess(ordersPaymentDTO.getOrderNumber());
+        return Result.success(orderPaymentVO);
+    }
+
 }
--- sky-server/src/main/java/com/sky/controller/admin/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/admin/user/UserController.java
@@ -1,22 +1,21 @@
 package com.sky.controller.admin.user;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.UserLoginDTO;
 import com.sky.entity.User;
 import com.sky.properties.JwtProperties;
 import com.sky.result.Result;
 import com.sky.service.UserService;
 import com.sky.utils.JwtUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.UserLoginVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -47,4 +46,5 @@ public class UserController {
                 .build();
         return Result.success(userLoginVO);
     }
+
 }
